e144165d3a0e11c04b30de5bba3b38fb0e42d926
Sindre Øye Svendby <sinsvend@gmail.com>
2018-05-12 10:51:11 +0200

    fix bug to render payment

--- pages/tournaments.js
+++ pages/tournaments.js
@@ -8,11 +8,11 @@ import Paper from "material-ui/Paper";
 import Typography from "material-ui/Typography";
 import Button from "material-ui/Button";
 import Grid from "material-ui/Grid";
-import Divider from 'material-ui/Divider';
+import Divider from "material-ui/Divider";
 import { withStyles } from "material-ui/styles";
 import withRoot from "../src/withRoot";
 import { getTournamentsInTheFuture } from "../src/api";
-import moment from 'moment';
+import moment from "moment";
 
 const CircularJSON = require("circular-json");
 const log = debug("tournaments");
@@ -88,82 +88,107 @@ class Tournaments extends React.Component {
           Turneringer
         </Typography>
         <Paper className={classes.tournamentPaper}>
-        <ul className={classes.tournamentList}>
-          {tournaments.map(
-            (
-                { id, name, deadline, startDate, classesText, playerVenue, shortNameProfixio },
-              index
-            ) => {
-              return (
-                <li key={id} className={classes.tournamentListItem}>
-
+          <ul className={classes.tournamentList}>
+            {tournaments.map(
+              (
+                {
+                  id,
+                  name,
+                  deadline,
+                  startDate,
+                  classesText,
+                  playerVenue,
+                  shortNameProfixio
+                },
+                index
+              ) => {
+                return (
+                  <li key={id} className={classes.tournamentListItem}>
                     <Grid container spacing={16}>
                       <Grid item xs={4} sm={2} md={1}>
-                          {renderDate(startDate)}
+                        {renderDate(startDate)}
                       </Grid>
                       <Grid item>
-                    <Typography variant="title">
-                      {name} - {playerVenue || ""}
-                    </Typography>
-                    <p>
-                      <Typography variant="body2">
-                        Påmeldingsfrist: {deadline}
-                      </Typography>
-                    </p>
-                    <p>
-                      <Typography variant="body1">
-                        Klasser: {classesText}
-                      </Typography>
-                    </p>
+                        <Typography variant="title">
+                          {name} - {playerVenue || ""}
+                        </Typography>
+                        <p>
+                          <Typography variant="body2">
+                            Påmeldingsfrist: {deadline}
+                          </Typography>
+                        </p>
+                        <p>
+                          <Typography variant="body1">
+                            Klasser: {classesText}
+                          </Typography>
+                        </p>
 
-                    <Link href={"/tournament"} as={`/tournaments/${id}`}>
-                      <Button color="primary">Påmeldte</Button>
-                    </Link>
-                      {renderSignupLink(id, deadline)}
-                      {shortNameProfixio && <a href={`https://www.profixio.com/matches/${shortNameProfixio}`}>
-                        <Button color="primary">Kampoppsett</Button>
-                      </a>}
+                        <Link href={"/tournament"} as={`/tournaments/${id}`}>
+                          <Button color="primary">Påmeldte</Button>
+                        </Link>
+                        {renderSignupLink(id, deadline)}
+                        {shortNameProfixio && (
+                          <a
+                            href={`https://www.profixio.com/matches/${shortNameProfixio}`}
+                          >
+                            <Button color="primary">Kampoppsett</Button>
+                          </a>
+                        )}
                       </Grid>
                     </Grid>
                     {index < tournaments.length - 1 && <Divider />}
-                </li>
-              );
-            }
-          )}
-        </ul>
+                  </li>
+                );
+              }
+            )}
+          </ul>
         </Paper>
       </React.Fragment>
     );
   }
 }
 
-
 function renderDate(date) {
   const [d, m, y] = date.split(".");
 
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Des'];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Des"
+  ];
 
   return (
-      <div>
-          <Typography variant="title">{d}</Typography>
-          <Typography variant="subheading">{months[parseInt(m-1)]}</Typography>
-      </div>
+    <div>
+      <Typography variant="title">{d}</Typography>
+      <Typography variant="subheading">{months[parseInt(m - 1)]}</Typography>
+    </div>
   );
-
 }
 
 function renderSignupLink(id, deadline) {
-
-    const timeToDeadLine = moment(deadline, "DD.MM.YYYY")
-        .endOf("day")
-        .diff(moment.now());
-    const signupAllowd = timeToDeadLine > 0;
-    if (signupAllowd) {
-      return (<Link href={"/signup"} as={`/signup/${id}`}>
+  const timeToDeadLine = moment(deadline, "DD.MM.YYYY")
+    .endOf("day")
+    .diff(moment.now());
+  const signupAllowd = timeToDeadLine > 0;
+  if (signupAllowd) {
+    // TOOD: client_token do not work when client side rendering, needs to be fixed.
+    // until that is fixed this is an a tag and not a Link tag.
+    return (
+      <a href={`/signup/${id}`}>
         <Button color="primary">Meld deg på</Button>
-      </Link>);
-    }
-    return null;
+      </a>
+    );
+  }
+  return null;
 }
 
 function renderTableData(props) {
