6946ad9a109f905ae34495371668aa7c8183c05c
Øystein Veien <oystein@netlife.no>
2019-05-16 09:46:15 +0200

    Changed profixio link to day view

--- components/TournamentListItem.js
+++ components/TournamentListItem.js
@@ -107,7 +107,7 @@ function renderDate(date) {
 function renderTournamentScheduleLink(shortNameProfixio, endDate) {
   if (shortNameProfixio && isTournamentDateOver(endDate)) {
     return (
-      <a href={`https://www.profixio.com/matches/${shortNameProfixio}`}>
+      <a href={`https://www.profixio.com/matches/${shortNameProfixio}/d`}>
         <Button color="primary">Kampresultater</Button>
       </a>
     );
